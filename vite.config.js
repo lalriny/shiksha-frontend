5fea68cf7a7ce40d5cf6f1a897e237ca615af161
Hruaia <hruaitluangasailo20@gmail.com>
2025-12-14 18:20:54 +0530

    sdsds

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+process.env.ROLLUP_USE_WASM = 'true';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
